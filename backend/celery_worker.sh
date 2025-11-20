a28fe8fd986cff1558865a0d630dfe3d9c64551d
Gayathri Ramanesh <gayathriramanesh@Gayathris-MacBook-Air.local>
2025-11-20 16:00:44 +0530

    Fixed celery worker

--- backend/celery_worker.sh
+++ backend/celery_worker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
 
-exec celery -A app.core.celery_app.celery_app worker \
-  --loglevel=INFO \
-  -Q csv-jobs
+exec celery -A app.core.celery_app:get_celery_app worker --loglevel=INFO -Q csv-jobs
+
